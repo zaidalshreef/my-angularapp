52c45e8aa13363b8abe214d2e5200700f7b3b457
zaid <zaidalshreef@gmail.com>
2021-05-27 12:37:00 +0300

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingComponent } from './shopping/shopping.component';
 import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingeditComponent } from './shopping/shoppingedit/shoppingedit.component';
+import { RecipeitemComponent } from './recipe/recipeitem/recipeitem.component';
+import { RecipedetailComponent } from './recipe/recipedetail/recipedetail.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [			
+  declarations: [
     AppComponent,
       HeaderComponent,
       ShoppingComponent,
-      RecipeComponent
+      RecipeComponent,
+      ShoppingeditComponent,
+      RecipedetailComponent,
+      RecipeitemComponent
+
+
    ],
   imports: [
     BrowserModule,
     FormsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
